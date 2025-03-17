f215e683b7027ad828bca882fa56baf479f01543
omar0940 <omar0940@gmail.com>
2025-03-17 19:02:44 +0100

    if Statment in Go

--- 04.Bank/01.bank.go
+++ 04.Bank/01.bank.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance = 1000.0
+
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check Balance")
@@ -13,6 +15,12 @@ func main() {
 	var choice int
 	fmt.Println("Enter your choice: ")
 	fmt.Scan(&choice)
+
+	// wantCheckBalance := choice == 1
+
+	if choice == 1{
+		fmt.Println("Your balance is", accountBalance)
+	}
 
 	fmt.Println("You chose", choice)
 
