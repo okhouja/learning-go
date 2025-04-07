0af8e0835cac42ec5db43930488422b0360a8365
omar0940 <omar0940@gmail.com>
2025-04-07 11:23:54 +0200

    add todo app to struct to work with interface

---
+++ 09.Todo_App_Project_Interface/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"example.com/note/note"
+	"example.com/note/todo"
+)
+
+func main(){
+	title, content := getNoteData()
+	todoText := getUserInput("Todo text:")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	todo.Display()
+	err = todo.Save()
+
+	if err != nil {
+		fmt.Println("Saving the todo failed.")
+		return
+	}
+	fmt.Println("Todo saved successfully!")
+	
+
+	userNote.Display()
+	err = userNote.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed.")
+		return
+	}
+
+	fmt.Println("Note saved successfully!")
+}
+
+
+
+func getNoteData() (string, string){
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string{
+	fmt.Printf("%v ",prompt)
+	// var value string	
+	// fmt.Scanln(&value) // good for one word input and not for multiple words
+
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n') // good for multiple words input
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+	
+	
+	return text
+}
