db437a49b0907fc92a19ee402490125f7c953fa6
omar0940 <omar0940@gmail.com>
2025-04-10 11:17:31 +0200

    working with type aliases

--- 11.Arrays_Slice_Maps/main.go
+++ 11.Arrays_Slice_Maps/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64 // Custom type (Type Aliases)for map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
 func main() {
 	// userNames := []string{}
 	userNames := make([]string, 2, 5) // 2 is the length, 5 is the capacity
@@ -12,4 +17,16 @@ func main() {
 	userNames = append(userNames, "Paul")
 
 	fmt.Println(userNames)
+
+	courseRatings := make(floatMap, 3) // 3 is the capacity
+
+	courseRatings["go"] = 4.7
+	courseRatings["react"] = 4.8
+	courseRatings["angular"] = 4.9
+	courseRatings["vue"] = 4.9
+
+	courseRatings.output()
+	
+	// fmt.Println(cap(userNames)) // Prints the capacity of the slice
+	// fmt.Println(len(courseRatings)) // Prints the number of key-value pairs in the map
 }
